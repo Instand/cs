d5d176eaa69a6468dddbfdc6b0f4d6b61bbd62a6
Arew <noobterr@yandex.ru>
2020-12-07 12:05:41 +0300

    Add basic usage test to thread pool.

--- tests/thread_pool_tests.cpp
+++ tests/thread_pool_tests.cpp
@@ -1,3 +1,35 @@
 #define TESTING
 
 #include <gtest/gtest.h>
+
+#include <cs/concurrent/thread_pool.hpp>
+
+TEST(ThreadPool, BaseUsage) {
+    static std::atomic<bool> called = { false };
+
+    cs::ThreadPool pool;
+    pool.enqueue([] {
+        called.store(true, std::memory_order_release);
+    });
+
+    size_t i = 0;
+
+    while (!called.load(std::memory_order_acquire) || i++ > 99) {
+        std::this_thread::yield();
+    }
+
+    ASSERT_TRUE(called.load(std::memory_order_acquire));
+}
+
+TEST(ThreadPool, BaseUsageFuture) {
+    static std::atomic<bool> called = { false };
+
+    cs::ThreadPool pool;
+    auto future = pool.enqueueFuture([] {
+        called.store(true, std::memory_order_release);
+    });
+
+    future.get();
+
+    ASSERT_TRUE(called.load(std::memory_order_acquire));
+}
