62d507925fa1b8530f634cfec4f0c18f1266f2bc
Arew <noobterr@yandex.ru>
2020-12-08 10:23:56 +0300

    Add simple safe logger.

---
+++ include/cs/logger/logger.hpp
@@ -0,0 +1,42 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <iostream>
+#include <sstream>
+
+namespace cs {
+// implements simple logging needs
+class Logger {
+public:
+    explicit Logger(const std::string& header = {}) {
+        if (!header.empty()) {
+            stream_ << header << ": ";
+        }
+    }
+
+    ~Logger() {
+        stream_ << '\n';
+        std::cout << stream_.str();
+    }
+
+    Logger& operator()() {
+        return *this;
+    }
+
+private:
+    std::stringstream stream_;
+
+    template<typename T>
+    friend Logger& operator<<(Logger& logger, const T& value);
+};
+
+template<typename T>
+inline Logger& operator<<(Logger& logger, const T& value) {
+    logger.stream_ << value;
+    return logger;
+}
+
+#define cslog Logger()
+}
+
+#endif // LOGGER_HPP
