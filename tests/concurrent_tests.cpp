6b625df50414121ef1b9c5d2a2395de6a9340b19
Arew <noobterr@yandex.ru>
2020-12-25 18:40:01 +0300

    Add concurrent run tests with result.

--- tests/concurrent_tests.cpp
+++ tests/concurrent_tests.cpp
@@ -5,11 +5,11 @@
 #include <cs/utils/waiter.hpp>
 #include <cs/concurrent/concurrent.hpp>
 
-static void concurrentBaseUsage(cs::RunPolicy policy) {
-    struct A {
-        std::atomic<bool> called = { false };
-    };
+struct A {
+    std::atomic<bool> called = { false };
+};
 
+static void concurrentBaseUsage(cs::RunPolicy policy) {
     A object;
 
     {
@@ -23,7 +23,24 @@ static void concurrentBaseUsage(cs::RunPolicy policy) {
     }
 
     cs::Waiter::wait([&] { return !object.called; }, 2000);
+    ASSERT_TRUE(object.called);
+}
 
+static void concurrentBaseUsageWithResult(cs::RunPolicy policy) {
+    A object;
+
+    {
+        auto watcher = cs::Concurrent::run(policy, [=] {
+            return policy;
+        });
+
+        cs::Connector::connect(&watcher->finished, [&](const cs::RunPolicy arg) {
+            object.called = true;
+            ASSERT_EQ(policy, arg);
+        });
+    }
+
+    cs::Waiter::wait([&] { return !object.called; }, 2000);
     ASSERT_TRUE(object.called);
 }
 
@@ -33,4 +50,12 @@ TEST(Concurrent, RunBaseUsageThreadPolicy) {
 
 TEST(Concurrent, RunBaseUsageThreadPoolPolicy) {
     concurrentBaseUsage(cs::RunPolicy::ThreadPool);
+}
+
+TEST(Concurrent, RunBaseUsageWithResultThreadPolicy) {
+    concurrentBaseUsageWithResult(cs::RunPolicy::Thread);
+}
+
+TEST(Concurrent, RunBaseUsageWithResultThreadPoolPolicy) {
+    concurrentBaseUsageWithResult(cs::RunPolicy::ThreadPool);
 }
